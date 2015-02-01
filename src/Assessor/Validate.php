38b55634b7e765549eed2fcda0a684f97f86e1d4
Christian Bolstad <christian@carnaby.se>
2015-02-01 02:59:55 +0100

    getTitle, supress all the warnings

--- src/Assessor/Validate.php
+++ src/Assessor/Validate.php
@@ -1,41 +1,53 @@
-<?php 
+<?php
 
 namespace Assessor;
 
 class Validate {
 
-	private $html; 
-	private $fetcher; 
-	private $doc; 
-	private $xpath; 
+	private $html;
+	private $fetcher;
+	private $doc;
+	private $xpath;
 
 	/**
-	 * Setup new object with static html or a remote url as parameter, pass it along to Fetch 
+	 * Setup new object with static html or a remote url as parameter, pass it along to Fetch
 	 * to get actual html data
-	 * @param string $data 
+	 *
+	 * @param string  $data
 	 */
 	function __construct( $data ) {
 		$this->fetcher = new Fetch( $data );
 		$this->html = $this->fetcher->getHtml();
 
-		// TODO - we might be forced to do some UTF-8 trickery here - see http://php.net/manual/en/domdocument.loadhtml.php#91513
-
 		$this->doc = new \DOMDocument();
-		$this->doc->loadHTML($this->html);
 
-		$this->xpath = new \DOMXpath($this->doc);
+		// To prevent our tests to choke on the warnings on malformed html we have to disable php warnings and build our own error stack
+		libxml_use_internal_errors( true );
+
+		$this->doc->loadHTML( $this->html );
+
+		// .. but since we dont' really care about broken html, we just throw the warnings away and clear our error stack
+		libxml_clear_errors();
+		libxml_use_internal_errors( false );
+
+		$this->xpath = new \DOMXpath( $this->doc );
 	}
 
 	/**
-	 * Return our html 
+	 * Return our html
+	 *
 	 * @return string
 	 */
 	function getHtml() {
-		return $this->html; 
+		return $this->html;
 	}
 
+	/**
+	 * Return the title
+	 *
+	 * @return string
+	 */
 	function getTitle() {
-		$titleNode = $this->xpath->query('//title');
-		return $titleNode->item(0);
+		return trim( $this->xpath->query( '//title' )->item( 0 )->textContent );
 	}
 }
