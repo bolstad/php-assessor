2f0d67ff117b3046d948481d47ddd684c03c2301
Christian Bolstad <christian@carnaby.se>
2015-02-01 01:33:56 +0100

    skip dummy function and build object, added getHtml()

--- src/Assessor/Validate.php
+++ src/Assessor/Validate.php
@@ -4,7 +4,24 @@ namespace Assessor;
 
 class Validate {
 
-	public static function world() {
-		return "helloz";
+	private $html; 
+	private $fetcher; 
+
+	/**
+	 * Setup new object with static html or a remote url as parameter, pass it along to Fetch 
+	 * to get actual html data
+	 * @param string $data 
+	 */
+	function __construct( $data ) {
+		$this->fetcher = new Fetch( $data );
+		$this->html = $this->fetcher->getHtml();
+	}
+
+	/**
+	 * Return our html 
+	 * @return string
+	 */
+	function getHtml() {
+		return $this->html; 
 	}
 }
