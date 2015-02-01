4756a30ed503d9efbef37b16abc5fe45f7a5ab04
Christian Bolstad <christian@carnaby.se>
2015-02-01 02:18:58 +0100

    parse tag

--- src/Assessor/Validate.php
+++ src/Assessor/Validate.php
@@ -6,6 +6,8 @@ class Validate {
 
 	private $html; 
 	private $fetcher; 
+	private $doc; 
+	private $xpath; 
 
 	/**
 	 * Setup new object with static html or a remote url as parameter, pass it along to Fetch 
@@ -15,6 +17,13 @@ class Validate {
 	function __construct( $data ) {
 		$this->fetcher = new Fetch( $data );
 		$this->html = $this->fetcher->getHtml();
+
+		// TODO - we might be forced to do some UTF-8 trickery here - see http://php.net/manual/en/domdocument.loadhtml.php#91513
+
+		$this->doc = new \DOMDocument();
+		$this->doc->loadHTML($this->html);
+
+		$this->xpath = new \DOMXpath($this->doc);
 	}
 
 	/**
@@ -23,5 +32,10 @@ class Validate {
 	 */
 	function getHtml() {
 		return $this->html; 
+	}
+
+	function getTitle() {
+		$titleNode = $this->xpath->query('//title');
+		return $titleNode->item(0);
 	}
 }
