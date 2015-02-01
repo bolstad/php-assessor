246cdc8f83d4a18ac448249e82a227b29e3ec552
Christian Bolstad <christian@carnaby.se>
2015-02-01 02:58:45 +0100

    Convert the content to UTF-8 when it's fetched

--- src/Assessor/Fetch.php
+++ src/Assessor/Fetch.php
@@ -21,8 +21,35 @@ class Fetch {
 		} else {
 			$fetched_data = $data;
 		}
+		$fetched_data = $this->toUtf8( $fetched_data );
 		$this->html = $fetched_data;
 	}
+
+	/**
+	 * Force html full page content conversion to utf-8, to be done before the DOM lib
+	 * does it stuff - code by 'piopier', http://php.net/manual/en/domdocument.loadhtml.php#91513
+	 *
+	 * @param string  $content String with a complete HTML page to be converted
+	 * @param string  $encod   Source encoding
+	 * @return string
+	 */
+	function toUtf8( $content, $encod='' ) {
+		mb_detect_order( "ASCII,UTF-8,ISO-8859-1,windows-1252,iso-8859-15" );
+		if ( !empty( $content ) ) {
+			if ( empty( $encod ) )
+				$encod  = mb_detect_encoding( $content );
+			$headpos        = mb_strpos( $content, '<head>' );
+			if ( FALSE=== $headpos )
+				$headpos= mb_strpos( $content, '<HEAD>' );
+			if ( FALSE!== $headpos ) {
+				$headpos+=6;
+				$content = mb_substr( $content, 0, $headpos ) . '<meta http-equiv="Content-Type" content="text/html; charset='.$encod.'">' .mb_substr( $content, $headpos );
+			}
+			$content=mb_convert_encoding( $content, 'HTML-ENTITIES', $encod );
+		}
+		return $content;
+	}
+
 
 	/**
 	 * Return fetched or passed data
